be1d8037881ad58a61213bf1dc7c5df243fd474a
xrodneylee <xrodneylee@gmail.com>
2017-09-02 20:56:07 +0800

    [feature] : : : login page

---
+++ src/components/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="row h-100 justify-content-center align-items-center">
+      <div class="card w-25" style="background-color:#212C3B">
+          <div class="card-body">
+              <form action="/dashboard" method="post">
+                  <div class="form-group">
+                      <input type="text" class="form-control" name="username" placeholder="Username" style="background-color:#1C2531">
+                  </div>
+                  <div class="form-group">
+                      <input type="password" class="form-control" name="password" placeholder="Password" style="background-color:#1C2531">
+                  </div>
+                  <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+html, body {
+  height: 100%;
+}
+</style>
