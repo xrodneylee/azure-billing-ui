ba2ad0672e97f2341672c832750aa768c0409764
xrodneylee <xrodneylee@gmail.com>
2017-09-19 00:34:12 +0800

    [enhance] column size

--- src/components/Setting.vue
+++ src/components/Setting.vue
@@ -5,21 +5,21 @@
       <button class="btn btn-outline-primary mt-3">新增</button>
       <table class="table mt-2">
         <thead>
-          <tr>
-            <th>#</th>
-            <th>tenant</th>
-            <th>client id</th>
-            <th>client secret</th>
-            <th></th>
+          <tr class="row">
+            <th class="col-1">#</th>
+            <th class="col-3">tenant</th>
+            <th class="col-3">client id</th>
+            <th class="col-3">client secret</th>
+            <th class="col-2"></th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            <td>
+          <tr class="row">
+            <th scope="row" class="col-1">1</th>
+            <td class="col-3">Mark</td>
+            <td class="col-3">Otto</td>
+            <td class="col-3">@mdo</td>
+            <td class="col-2">
               <button class="btn btn-outline-danger">刪除</button>
               <button class="btn btn-outline-warning">訂用帳戶</button>
             </td>
