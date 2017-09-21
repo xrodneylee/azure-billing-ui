febe96ca1bcb30243ee83eee90cb5feeca8909c6
xrodneylee <xrodneylee@gmail.com>
2017-09-22 00:13:23 +0800

    [enhance] clean

--- src/App.vue
+++ src/App.vue
@@ -14,18 +14,13 @@ export default {
 @import "~bootstrap/scss/bootstrap";
 @import "./assets/css/font-awesome.min.css";
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
-}
 .page-container {
   position: absolute;
-  /* left: 0;
-  top: 0; */
   width: 100%;
   height: 100%;
   background-color: #131920;
+}
+button {
+  cursor: pointer;
 }
 </style>
